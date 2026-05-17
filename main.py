15ffe9e3f293133f969a4ca1d6baa6aaed3b31d0
Damir <daririulets@gmail.com>
2026-05-17 11:14:09 +0300

    input

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pygame
 # ;D
 
 graph_flag = 1
+inputi = "x^2+y^2"
+graph_input = inputi.replace("^", "**")
+raw_eq = "50^2"
+eq = raw_eq.replace("^", "**")
 
 pygame.init()
 graph = pygame.display.set_mode((800, 600))
