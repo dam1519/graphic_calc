fbcefe0c14fdb47fe4a74d14acf3df0f30fec79a
Damir <daririulets@gmail.com>
2026-05-29 19:50:09 +0300

    0.31

--- main.py
+++ main.py
@@ -1,93 +1,91 @@
 import pygame
 import math
 
-# spagheti very good spagheti/ спагети очень хорошие спагети
-
-class GraphEngine:
-    def __init__(self, graph, eq):
+class GraphicEngine:
+    def __init__(self, left_side, right_side):
         self.graph_flag = 1
-        self.inputi = graph
+        self.inputi = left_side
         self.graph_input = self.inputi.replace("^", "**")
-        self.raw_eq = eq
+        self.raw_eq = right_side
         self.eq = self.raw_eq.replace("^", "**")
         self.contx = {"sin": math.sin,
-                     "cos": math.cos,
-                     "tan": math.tan,
-                     "max": max,
-                     "min": min,
-                     "abs": abs,
-                     "pi": math.pi,
-                     "e": math.e}
-
-    def Optimisation(self):
+                 "cos": math.cos,
+                 "tan": math.tan,
+                 "max": max,
+                 "min": min,
+                 "abs": abs,
+                 "pi": math.pi,
+                 "e": math.e}
         self.compil_graph = compile(self.graph_input, '<string>', 'eval')
         self.compil_eq = compile(self.eq, '<string>', 'eval')
-        self.pixel = pygame.PixelArray(self.graph)
+        self.points = []
 
-    def PygInit(self, name, setmode, fillcolr):
-        pygame.init()
-        self.graph = pygame.display.set_mode(setmode)
-        pygame.display.set_caption(name)
-        self.clock = pygame.time.Clock()
-        self.graph.fill(fillcolr)
-
-    def Calculation(self, x, y):
-        self.x = x
-        self.y = y
-        self.contx["x"] = self.x
-        self.contx["y"] = self.y
-        self.result = eval(self.compil_graph, {}, self.contx)
-        self.result_eq = eval(self.compil_eq, {}, self.contx)
-
-        self.contx["x"] = self.x + 1
-        self.raw_nextx = eval(self.compil_graph, {}, self.contx)
-
-        self.contx["x"] = self.x
-        self.contx["y"] = self.y + 1
-        self.raw_nexty = eval(self.compil_graph, {}, self.contx)
-        self.nextx = self.raw_nextx - self.result
-        self.nexty = self.raw_nexty - self.result
-        self.thickness = (self.nextx ** 2 + self.nexty ** 2) ** 0.5
-
-    def Render(self):
-        if self.thickness > 0 and abs(self.result - self.result_eq) / self.thickness < 0.99:
-            self.pixel[self.x + 400, -self.y + 300] = (255, 0, 0)
-# ;D
-
-calc = GraphEngine("max(abs(x),abs(y))", "50")
-
-calc.PygInit("графический калькулятор 3.0", (800, 600), (0, 0, 0))
-
-calc.Optimisation()
-
-
-
-
-
-
-running = True
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    if calc.graph_flag == 1:
+    def Calculation(self):
         for x in range(-400, 400):
             for y in range(-300, 300):
                 try:
-                    calc.Calculation( x, y)
-                    calc.Render()
+                    self.contx["x"] = x
+                    self.contx["y"] = y
+                    self.result = eval(self.compil_graph, {}, self.contx)
+                    self.result_eq = eval(self.compil_eq, {}, self.contx)
+
+                    self.contx["x"] = x + 1
+                    self.raw_nextx = eval(self.compil_graph, {}, self.contx)
+
+                    self.contx["x"] = x
+                    self.contx["y"] = y + 1
+                    self.raw_nexty = eval(self.compil_graph, {}, self.contx)
+                    self.nextx = self.raw_nextx - self.result
+                    self.nexty = self.raw_nexty - self.result
+                    self.thickness = (self.nextx ** 2 + self.nexty ** 2) ** 0.5
+                    if self.thickness > 0 and abs(self.result - self.result_eq) / self.thickness < 0.99:
+                       self.points.append((x, y))
                 except:
                     continue
+        return self.points
 
+class Canvas:
+    def __init__(self):
+        pygame.init()
+        self.graph = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("графический калькулятор 0.22")
+        self.clock = pygame.time.Clock()
+        self.graph.fill((0, 0, 0))
+        self.pixel = pygame.PixelArray(self.graph)
+
+    def Render(self, points):
+        for i in points:
+            self.pixel[i[0] + 400, -i[1] + 300] = (255, 0, 0)
             pygame.display.update()
 
-        calc.graph_flag = 0
+class Running:
+    def __init__(self, graph, eq):
+        self.canvas = Canvas()
+        self.graph = GraphicEngine(graph, eq)
+        self.graph_flag = 1
+        self.running = True
+        self.clock = pygame.time.Clock()
+
+    def Run(self):
+        self.running = True
+        while self.running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            if self.graph_flag == 1:
+                points = self.graph.Calculation()
+                self.canvas.Render(points)
+            self.graph_flag = 0
+
+            pygame.display.flip()
+
+            self.clock.tick(60)
+
+        pygame.quit()
 
 
-    pygame.display.flip()
 
-    calc.clock.tick(60)
-
-pygame.quit()
+run = Running("max(abs(x), abs(y))", "7^2")
+run.Run()
